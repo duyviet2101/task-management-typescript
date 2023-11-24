468a70825f28e0bbb2c993a36b8cb1213a9cde7f
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-11-25 00:45:25 +0700

    tạo routé

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import * as database from "./config/database";
 import dotenv from 'dotenv';
-import Task from './models/task.model';
+import mainV1Routes from './api/v1/routes/index.route';
 dotenv.config();
 
 const app: Express = express();
@@ -9,24 +9,7 @@ const port: number | string = process.env.PORT || 3009;
 
 database.connect();
 
-
-app.get('/tasks', async (req: Request, res: Response) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
-    res.json(tasks);
-});
-
-app.get('/tasks/detail/:id', async (req: Request, res: Response) => {
-    const id = req.params.id;
-
-    const task = await Task.findOne({
-        _id: id,
-        deleted: false
-    });
-
-    res.json(task);
-});
+mainV1Routes(app);
 
 app.listen(port, () => {
     console.log(`WSV listening at http://localhost:${port}`);
