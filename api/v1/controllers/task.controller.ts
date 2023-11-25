3aef43e7164140d5233d644feba99001a42a2881
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-11-25 19:27:55 +0700

    pagination

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import Task from '../models/task.model';
+import paginationHelper from '../../../helpers/pagination';
 
 export const index = async (req: Request, res: Response) => {
-    //!Find interface
+    //?Find interface
     interface Find {
         deleted: boolean;
         status?: string;
@@ -15,17 +16,32 @@ export const index = async (req: Request, res: Response) => {
     if (req.query.status) {
         find['status'] = req.query.status.toString();
     }
-    //!END Find interface
+    //?END Find interface
 
-    //! Sort
+    //? Sort
     const sort = {};
     if (req.query.sortKey && req.query.sortValue) {
         sort[req.query.sortKey.toString()] = req.query.sortValue.toString();
     }
-    //! End Sort
+    //? End Sort
+
+    //? pagination
+    const initPagination = {
+        currentPage: 1,
+        limitItems: 2
+    }
+    const countTask = await Task.countDocuments(find)
+    const objectPagination = paginationHelper(
+        initPagination,
+        req.query,
+        countTask
+    )
+    //? end pagination
 
     const tasks = await Task.find(find)
-        .sort(sort);
+        .sort(sort)
+        .skip(objectPagination.skip)
+        .limit(objectPagination.limitItems)
     res.json(tasks);
 }
 
