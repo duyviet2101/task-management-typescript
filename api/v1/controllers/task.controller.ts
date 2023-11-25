c3e86f2cbdda0c3a1e898335bc1207ce1a82cae5
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-11-25 19:16:41 +0700

    sort

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import Task from '../models/task.model';
 
 export const index = async (req: Request, res: Response) => {
+    //!Find interface
     interface Find {
         deleted: boolean;
         status?: string;
@@ -14,8 +15,17 @@ export const index = async (req: Request, res: Response) => {
     if (req.query.status) {
         find['status'] = req.query.status.toString();
     }
+    //!END Find interface
 
-    const tasks = await Task.find(find);
+    //! Sort
+    const sort = {};
+    if (req.query.sortKey && req.query.sortValue) {
+        sort[req.query.sortKey.toString()] = req.query.sortValue.toString();
+    }
+    //! End Sort
+
+    const tasks = await Task.find(find)
+        .sort(sort);
     res.json(tasks);
 }
 
