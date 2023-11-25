086681febb1c7d692b059ef9b31241ee19a3185f
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-11-25 20:35:32 +0700

    get detail

--- api/v1/controllers/user.controller.ts
+++ api/v1/controllers/user.controller.ts
@@ -67,3 +67,19 @@ export const login = async (req: Request, res: Response) => {
         }
     })
 }
+
+// [GET] api/v1/users/detail/:id
+export const detail = async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+
+    const user = await User.findOne({
+        _id: id,
+        deleted: false
+    }).select("-password -token -deleted -deletedAt -createdAt -updatedAt");
+
+    res.json({
+        code: 200,
+        message: "success",
+        info: user
+    })
+}
